a3bad06419edd85dbe605e5e14c8aeb55f665556
ShaDisNX255 <38537144+ShaDisNX255@users.noreply.github.com>
2025-06-20 23:18:09 -0500

    Major Update
    -New area, Ice Rink

---
+++ assets/tiles/ShademanCompWarpAnimation.tsx
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.11.2" name="ShademanCompWarpAnimation" tilewidth="56" tileheight="27" tilecount="6" columns="6" objectalignment="top">
+ <tileoffset x="0" y="-1"/>
+ <image source="ShademanCompWarpAnimation.png" width="336" height="27"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="30" y="0">
+    <polygon points="0,0 26,13 0,27 -4,27 -30,14 -4,0"/>
+   </object>
+  </objectgroup>
+  <animation>
+   <frame tileid="0" duration="200"/>
+   <frame tileid="1" duration="200"/>
+   <frame tileid="2" duration="200"/>
+   <frame tileid="3" duration="200"/>
+   <frame tileid="4" duration="200"/>
+   <frame tileid="5" duration="200"/>
+   <frame tileid="5" duration="200"/>
+   <frame tileid="4" duration="200"/>
+   <frame tileid="3" duration="200"/>
+   <frame tileid="2" duration="200"/>
+   <frame tileid="1" duration="200"/>
+   <frame tileid="0" duration="200"/>
+  </animation>
+ </tile>
+</tileset>
