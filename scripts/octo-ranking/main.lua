c3b3c024a4f44fd35a0720f3e6d6e81a536bf6e2
ShaDisNX255 <rayo_dis_nx@hotmail.com>
2025-06-24 13:01:44 +0100

    Update to gitignore

--- scripts/octo-ranking/main.lua
+++ scripts/octo-ranking/main.lua
@@ -17,7 +17,8 @@ local players_in_unranked_matchmaking = {}
 local players_in_ranked_matchmaking = {}
 local player_challenges = {}
 local bbs_type = {}
-
+local players_in_battle = {}
+local timer = 0
 local function find_in_table(t,v1)
 	for i,v2 in pairs(t) do
 		if v1 == v2 then
@@ -46,7 +47,6 @@ local function check_areas()
 	local areas = Net.list_areas()
     for i, area_id in next, areas do
         local area_id = tostring(area_id)
-		print(Net.get_area_custom_property(area_id, "OctoPVP"))
 		if Net.get_area_custom_property(area_id, "OctoPVP") == "true" then
 			pvp_areas[area_id] = true
 		elseif Net.get_area_custom_property(area_id, "OctoPVP") == "false" then
@@ -55,7 +55,6 @@ local function check_areas()
 			print("The value of OctoPVP in "..area_id.. " is invalid.")
 		end
 	end
-	print(pvp_areas)
 end
 
 local function save_file(file_path)
@@ -188,13 +187,25 @@ Net:on("actor_interaction", function(event)
 		emitter:on("post_selection", function(event)
 			if event.post_id == "Challenge1" then
 				player_challenges[player_id] = actor_id
-				Net.exclusive_player_emote(player_id, actor_id, 7)
+				Net.exclusive_player_emote(actor_id, player_id, 7)
+				Net.exclusive_player_emote(player_id, player_id, 7)
+
+				Net.close_bbs(player_id)
 			elseif event.post_id == "Challenge2" then
 				player_challenges[actor_id] = nil
 				Net.initiate_pvp(player_id,actor_id)
+				players_in_battle[player_id] = actor_id
+    			players_in_battle[actor_id] = player_id
 			end
 		end)
 	end 
+end)
+
+Net:on("battle_results", function(event)
+  --Taken from Keristero's pvp_with_stats.lua
+  if players_in_battle[event.player_id] then
+      players_in_battle[event.player_id] = nil
+  end
 end)
 
 Net:on("player_disconnect", function(event)
@@ -211,6 +222,24 @@ Net:on("player_area_transfer", function(event)
 		pcall(function() table.remove(players_in_unranked_matchmaking,find_in_table(players_in_unranked_matchmaking,player_id)) end)
 		pcall(function() table.remove(players_in_ranked_matchmaking,find_in_table(players_in_ranked_matchmaking,player_id)) end)
 	end
+end)
+
+Net:on("player_disconnect", function(event)
+  local player_id = event.player_id
+  players_in_battle[player_id] = nil
+end)
+
+Net:on("tick", function(event)
+	--Taken from Keristero's pvp_with_stats.lua
+    timer = timer + event.delta_time
+    if timer > 5 then
+        for player_id, value in pairs(players_in_battle) do
+			if players_in_battle[player_id] ~= nil then 
+            	Net.set_player_emote(player_id, 7) --swords emote
+			end
+        end  
+        timer = 0
+    end
 end)
 
 Net:on("post_selection", function(event)
