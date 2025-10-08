b1847e0fdac1d7052785ed6adc948765eb7cc42d
ShaDisNX255 <38537144+ShaDisNX255@users.noreply.github.com>
2025-10-08 12:55:37 -0500

    Update to decor items
    - Added Bass plush!

--- assets/objects/BoktaiDolls28x33.tsx
+++ assets/objects/BoktaiDolls28x33.tsx
@@ -1,39 +1,39 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="BoktaiDolls28x33" tilewidth="28" tileheight="30" tilecount="5" columns="5" objectalignment="bottom">
+<tileset version="1.8" tiledversion="1.11.2" name="BoktaiDolls28x33" tilewidth="28" tileheight="30" tilecount="5" columns="5" objectalignment="bottom">
  <tileoffset x="5" y="0"/>
  <image source="BoktaiDolls28x33.png" trans="ffffff" width="141" height="32"/>
  <tile id="0">
   <objectgroup draworder="index" id="2">
-   <object id="3" x="0" y="11" width="23" height="19">
-    <ellipse/>
+   <object id="4" x="24" y="21">
+    <polygon points="0,0 0,9 -19,9 -28,1 -22,-5"/>
    </object>
   </objectgroup>
  </tile>
  <tile id="1">
   <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="7" width="28" height="23">
-    <ellipse/>
+   <object id="2" x="28" y="21">
+    <polygon points="0,0 0,9 -19,9 -28,1 -22,-5"/>
    </object>
   </objectgroup>
  </tile>
  <tile id="2">
   <objectgroup draworder="index" id="2">
-   <object id="3" x="2" y="9" width="28" height="23">
-    <ellipse/>
+   <object id="4" x="28" y="21">
+    <polygon points="0,0 0,9 -19,9 -28,1 -22,-5"/>
    </object>
   </objectgroup>
  </tile>
  <tile id="3">
   <objectgroup draworder="index" id="2">
-   <object id="1" x="2" y="10" width="23" height="19">
-    <ellipse/>
+   <object id="2" x="27" y="21">
+    <polygon points="0,0 0,9 -19,9 -28,1 -22,-5"/>
    </object>
   </objectgroup>
  </tile>
  <tile id="4">
   <objectgroup draworder="index" id="2">
-   <object id="5" x="0" y="8" width="26" height="22">
-    <ellipse/>
+   <object id="8" x="28" y="21">
+    <polygon points="0,0 0,9 -19,9 -28,1 -22,-5"/>
    </object>
   </objectgroup>
  </tile>
---
+++ assets/objects/bass_plush.tsx
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.11.2" name="bass_plush" tilewidth="32" tileheight="32" tilecount="1" columns="1" objectalignment="bottom">
+ <image source="bass_plush.png" width="32" height="32"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="3">
+   <object id="4" x="29" y="32">
+    <polygon points="0,0 -25,0 -29,-10 -20,-13 1,-7"/>
+   </object>
+  </objectgroup>
+ </tile>
+</tileset>
