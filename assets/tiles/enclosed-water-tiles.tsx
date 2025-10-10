be7c55f991a5ace839e43084d2f026aaae4b1dc4
ShaDisNX255 <38537144+ShaDisNX255@users.noreply.github.com>
2025-10-10 06:32:38 -0500

    Fishing mini-game polish
    - Better looking tiles
    - Modified code to work on other areas

---
+++ assets/tiles/enclosed-water-tiles.tsx
@@ -0,0 +1,64 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="enclosed-water-tiles" tilewidth="64" tileheight="36" tilecount="32" columns="8">
+ <image source="enclosed-water-tiles.png" width="512" height="144"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="333"/>
+   <frame tileid="16" duration="333"/>
+  </animation>
+ </tile>
+ <tile id="1">
+  <animation>
+   <frame tileid="1" duration="333"/>
+   <frame tileid="17" duration="333"/>
+  </animation>
+ </tile>
+ <tile id="8">
+  <animation>
+   <frame tileid="8" duration="333"/>
+   <frame tileid="24" duration="333"/>
+  </animation>
+ </tile>
+ <tile id="9">
+  <animation>
+   <frame tileid="9" duration="333"/>
+   <frame tileid="25" duration="333"/>
+  </animation>
+ </tile>
+ <tile id="10">
+  <animation>
+   <frame tileid="10" duration="333"/>
+   <frame tileid="26" duration="333"/>
+  </animation>
+ </tile>
+ <tile id="11">
+  <animation>
+   <frame tileid="11" duration="333"/>
+   <frame tileid="27" duration="333"/>
+  </animation>
+ </tile>
+ <tile id="12">
+  <animation>
+   <frame tileid="12" duration="333"/>
+   <frame tileid="28" duration="333"/>
+  </animation>
+ </tile>
+ <tile id="13">
+  <animation>
+   <frame tileid="13" duration="333"/>
+   <frame tileid="29" duration="333"/>
+  </animation>
+ </tile>
+ <tile id="14">
+  <animation>
+   <frame tileid="14" duration="333"/>
+   <frame tileid="30" duration="333"/>
+  </animation>
+ </tile>
+ <tile id="15">
+  <animation>
+   <frame tileid="15" duration="333"/>
+   <frame tileid="31" duration="333"/>
+  </animation>
+ </tile>
+</tileset>
