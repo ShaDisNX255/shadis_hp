8a8fffbc939de7821bc98c671ebc0ff7928ac69a
ShaDisNX255 <38537144+ShaDisNX255@users.noreply.github.com>
2025-11-28 20:46:40 -0600

    Push zary doll
    Missing asset

---
+++ assets/objects/Zarydoll.tsx
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Zarydoll" tilewidth="30" tileheight="28" tilecount="1" columns="1">
+ <image source="Zarydoll.png" width="30" height="28"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="14">
+    <polygon points="0,0 0,14 30,14 30,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+</tileset>
