bc26c0cc16f63cb68b8346b9a2b4ef86183151f6
ShaDisNX255 <38537144+ShaDisNX255@users.noreply.github.com>
2026-02-03 07:53:07 -0600

    Update to prizes for February

--- scripts/teams/month.lua
+++ scripts/teams/month.lua
@@ -96,6 +96,37 @@ Month.MONTHS = {
       },
     },
   },
+
+  -- January 2026
+  ["2026-01"] = {
+    top_player = {
+      items_inline = {
+        { type="item", name="[UR]F.A.REBD", description="FullArt: Red Eyes Black Dragon - A: 2400 / D: 2000", amount=1 }
+      }
+    },
+    team_win = {
+       cosmetics = {
+         { id = "Lightning1", label = "Lightning1" },
+       },
+    },
+    losing_team = {
+      pack_name = "Team Pack Vol. 1",
+      pack_rolls = 1,   -- how many random cards to give from pack_pool
+      pack_pool = {
+        -- weight is relative odds; amount is how many copies of that card to give
+        { type = "item", name = "[SR]Gaia",       description = "SRare: Gaia the Fierce Knight - A: 2300 / D: 2100", amount = 1, weight = 20 },
+        { type = "item", name = "[SR]REBMD",      description = "SRare: Red-Eyes Black Metal Dragon - A: 2800 / D: 2400", amount = 1, weight = 20 },
+        { type = "item", name = "[SR]REBD",       description = "SRare: Red Eyes Black Dragon - A: 2400 / D: 2000", amount = 1, weight = 20 },
+        { type = "item", name = "[UR]S.Skull",    description = "URare: Summoned Skull - A: 2500 / D: 1200", amount = 1, weight = 20 },
+        { type = "item", name = "[GR]DMGirl",     description = "GRare: Dark Magician Girl - A: 2000 / D: 1700", amount = 1, weight = 4 },
+        { type = "item", name = "[GR]DMag",       description = "GRare: Dark Magician - A: 2500 / D: 2100", amount = 1, weight = 4 },
+        { type = "item", name = "[GDR]S.Skull",   description = "GDRare: Summoned Skull - A: 2500 / D: 1200", amount = 1, weight = 2 },
+        { type = "item", name = "[SR]F.A.DMGirl", description = "FullArt: Dark Magician Girl - A: 2000 / D: 1700", amount=1, weight = 2}
+        { type = "item", name = "[UR]F.A.DMag",   description = "FullArt: Dark Magician - A: 2500 / D: 2100", amount=1, weight = 2}
+      },
+    },
+  },
+}
 }
 
 -- ===== Dated special events (independent of monthly rewards) =====
@@ -131,10 +162,10 @@ Month.EVENTS = {
    }
   },
   {
-   id   = "PrizeTest-251225",
+   id   = "PrizeTest-260203",
    name = "Prize Test",
-   start = "2025-12-21",
-   ["end"] = "2025-12-21",
+   start = "2026-02-03",
+   ["end"] = "2026-02-03",
    rewards = {
      cosmetics = {
        { id = "Lightning1", label = "Lightning1" },
