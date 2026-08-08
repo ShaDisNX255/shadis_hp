f4141bc4eab9d943684b0ed299f9fe6d476e9d37
ShaDisNX255 <38537144+ShaDisNX255@users.noreply.github.com>
2026-08-08 11:25:40 -0500

    Adding an UNO table in our hangout
    Yes

---
+++ assets/uno/uno_table.tsx
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="uno_table" tilewidth="64" tileheight="41" tilecount="1" columns="1" objectalignment="bottom">
+ <tileoffset x="10" y="10"/>
+ <image source="uno_table.png" width="64" height="41"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-8" y="25">
+    <polygon points="0,0 43,22 75,5 37,-17"/>
+   </object>
+  </objectgroup>
+ </tile>
+</tileset>
